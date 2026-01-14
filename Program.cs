5c0c3c9ed0ec7942fa22a0496ccf55c460806ee3
Sebastiaan Janssen <sebastiaan@umbraco.com>
2026-01-14 15:56:28 +0100

    Move DataProtection path to Program.cs otherwise it might not be added on time

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.AspNetCore.DataProtection;
 using UmbraCalendar.Jobs;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Persist Data Protection keys to a stable location for Docker
+// This ensures OAuth tokens and antiforgery tokens can be decrypted after container restarts
+var keysDirectory = Path.Combine(AppContext.BaseDirectory, "umbraco", "Data", "DataProtection-Keys");
+Directory.CreateDirectory(keysDirectory);
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(keysDirectory));
 
 builder.CreateUmbracoBuilder()
     .AddBackOffice()
--- RegisterServices.cs
+++ RegisterServices.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.DataProtection;
-using UmbraCalendar.Database;
+﻿using UmbraCalendar.Database;
 using UmbraCalendar.Meetup;
 using Umbraco.Cms.Core.Composing;
 
@@ -13,13 +12,6 @@ public class RegisterServices : IComposer
         builder.Services.AddSingleton<Options>();
         builder.Services.AddSingleton<IMeetupService, MeetupService>();
         builder.Services.AddSingleton<IUpcomingMeetupService, UpcomingMeetupService>();
-
-        // Persist Data Protection keys to a stable location for Docker
-        // This ensures OAuth tokens can be decrypted after container restarts
-        var keysDirectory = Path.Combine(AppContext.BaseDirectory, "umbraco", "Data", "DataProtection-Keys");
-        Directory.CreateDirectory(keysDirectory);
-        builder.Services.AddDataProtection()
-            .PersistKeysToFileSystem(new DirectoryInfo(keysDirectory));
 
         var externalSettingsSection = builder.Config.GetSection("ExternalServices");
         builder.Services.Configure<Options>(externalSettingsSection);
